8b3a1afa075b2e05346564ccf0ff7a14d7778f45
Azbad <bazeezd4917@gmail.com>
2023-12-13 18:09:02 -0800

    commit for shell_main.c

--- command_handler.c
+++ command_handler.c
@@ -5,8 +5,9 @@
  * @tokenized_command: command entered
  * Return: void
  */
-void env(char **tokenized_command) {
-	(void)tokenized_command;  // Unused parameter
+void env(char **tokenized_command)
+{
+	(void)tokenized_command;  /* Unused parameter */
 	int i;
 
 	for (i = 0; environ[i] != NULL; i++)
@@ -21,7 +22,8 @@ void env(char **tokenized_command) {
  * @tokenized_command: command entered
  * Return: void
  */
-void quit(char **tokenized_command) {
+void quit(char **tokenized_command)
+{
 	int num_token = 0, arg;
 
 	for (; tokenized_command[num_token] != NULL; num_token++)
@@ -33,9 +35,11 @@ void quit(char **tokenized_command) {
 		free(line);
 		free(commands);
 		exit(status);
-	} else if (num_token == 2) {
+	} else if (num_token == 2)
+	{
 		arg = _atoi(tokenized_command[1]);
-		if (arg == -1) {
+		if (arg == -1)
+		{
 			print(shell_name, STDERR_FILENO);
 			print(": 1: exit: Illegal number: ", STDERR_FILENO);
 			print(tokenized_command[1], STDERR_FILENO);
@@ -49,8 +53,10 @@ void quit(char **tokenized_command) {
 			free(commands);
 			exit(arg);
 		}
-	} else {
-			print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+	}
+	else
+	{
+		print("$: exit doesn't take more than one argument\n", STDERR_FILENO);
 	}
 }
 
--- shell_main.c
+++ shell_main.c
@@ -1,12 +1,3 @@
-#include "shell.h"
-#include "utility_function.c"
-
-char *line = NULL;
-char **commands = NULL;
-char *shell_name;
-int status = 0;
-
-
 int main(int argc __attribute__((unused)), char **argv)
 {
 	char **current_command = NULL;
@@ -15,67 +6,81 @@ int main(int argc __attribute__((unused)), char **argv)
 	signal(SIGINT, ctrl_c_handler);
 	shell_name = argv[0];
 
-/* Check if the shell is running interactively */
-if (isatty(STDIN_FILENO))
-{
-/* Interactive mode */
-while (1)
-{
-	print(" Basic Shell >$ ", STDOUT_FILENO);
-
-			if (getline(&line, &n, stdin) == -1)
-			{
-				free(line);
-				exit(status);
-			}
-
-			remove_newline(line);
-			remove_comment(line);
-			commands = tokenizer(line, ";");
-
-			for (int i = 0; commands[i] != NULL; i++)
-			{
-				current_command = tokenizer(commands[i], " ");
-
-				if (current_command[0] == NULL)
-				{
-					free(current_command);
-					break;
-
-				}
-
-				initializer(current_command, parse_command(current_command[0]));
-				free(current_command);
-				}
-
-			free(commands);
-			}
-			else
-			{
-/* Non-interactive mode */
-		non_interactive();
+	/* Check if the shell is running interactively */
+	if (isatty(STDIN_FILENO))
+	{
+		/* Interactive mode */
+		while (1)
+		{
+			interactive_mode();
 		}
+	}
+	else
+	{
+		/*
+		 *  Non-interactive_mode
+		 */;
+		non_interactive_mode();
+	}
 	return (status);
 }
-
 void interactive_mode(void)
 {
+	print(" ($) ", STDOUT_FILENO);
 
+	if (getline(&line, &n, stdin) == -1)
+	{
+		free(line);
+		exit(status);
+	}
+
+	remove_newline(line);
+	remove_comment(line);
+	commands = tokenizer(line, ";");
+
+	for (int i = 0; commands[i] != NULL; i++)
+	{
+		current_command = tokenizer(commands[i], " ");
+
+		if (current_command[0] == NULL)
+		{
+			free(current_command);
+			break;
+		}
+		initializer(current_command, parse_command(current_command[0]));
+		free(current_command);
+	}
+
+	free(commands);
 }
-
 void non_interactive_mode(void)
 {
+	char **current_command = NULL;
+	int i;
+	size_t n = 0;
 
+	while (getline(&line, &n, stdin) != -1)
+	{
+		remove_newline(line);
+		remove_comment(line);
+		commands = tokenizer(line, ";");
+
+		for (i = 0; commands[i] != NULL; i++)
+		{
+		current_command = tokenizer(commands[i], " ");
+
+	if (current_command[0] == NULL)
+	{
+	free(current_command);
+	break;
+	}
+
+	initializer(current_command, parse_command(current_command[0]));
+	free(current_command);
+		}
+		free(commands);
+	}
+
+	free(line);
+	exit(status);
 }
-
-void initializer(char **current_command, int type_command)
-{
-
-}
-
-int parse_command(char *command)
-{
-	return (0);
-}
-
-
